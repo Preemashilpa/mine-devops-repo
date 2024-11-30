12e47febd07876524f9e91c0fd3af0428c22baca
pshilpadsouza <pshilpadsouza.com>
2024-11-30 12:32:16 +0530

    Updated ami

--- terraform1/main.tf
+++ terraform1/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "instance-1" {
-    ami = "ami-0453ec754f44f9a4a"
+    ami = "ami-0614680123427b75e"
     instance_type = "t2.micro"
     security_groups = ["default"]
     key_name = "winkey-25-09"
@@ -14,7 +14,7 @@ resource "aws_instance" "instance-1" {
 }
 
 resource "aws_instance" "instance-2" {
-    ami = "ami-0453ec754f44f9a4a"
+    ami = "ami-0614680123427b75e"
     instance_type = "t2.micro"
     security_groups = ["default"]
     key_name = "winkey-25-09"
